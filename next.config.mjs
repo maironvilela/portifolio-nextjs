69d0aa3701428cf76c36fa5bdf345d5003e15d7b
mairon.vilela <maironvilela@hotmail.com>
2024-08-08 14:08:27 -0300

    fix: fonts

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@ const nextConfig = {
   images: {
     domains: ['yt3.ggpht.com', 'th.bing.com', 'sa-east-1.graphassets.com'],
   },
-
-  staticPageGenerationTimeout: 800,
-  optimizeFonts: false,
-
 }
 
 export default nextConfig
